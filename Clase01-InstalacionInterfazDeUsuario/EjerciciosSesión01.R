5d28fa76cde64af81a601c07a63a7b6632d9ddd4
Cynthia Castillo <73755248+LaMatemaga@users.noreply.github.com>
2021-09-13 15:41:32 -0500

    Add files via upload

---
+++ Clase01-InstalacionInterfazDeUsuario/EjerciciosSesión01.R
@@ -0,0 +1,13 @@
+getwd()
+setwd("C:/Users/Elaia/Documents/Academia/Academia de Ciencias/1 - Introducción a R")
+getwd()
+
+list.files()
+list.dirs()
+fa
+ls()
+ls() == "taller"
+sum(ls() == "taller")
+sum(ls() == "libros")
+rm(lista)
+ls()
